475a198dcc46ae5ccfbaf80430ad0189811695ed
fumiwolf <fumi01200924@gmail.com>
2018-06-24 01:11:37 +0900

    add IVR_TimeReport2

--- IVR_NFC8.py
+++ IVR_NFC8.py
@@ -4,7 +4,6 @@ import binascii
 import csv
 import sys
 import time
-import pygame.mixer
 import json
 import codecs
 import PlayVoice
@@ -26,17 +25,17 @@ def clockIn():
     writeList.append(now.strftime("%Y-%m-%d %A"))
     writeList.append(now.strftime("%X"))
     writeList.append(now.strftime("%s"))
-    print("書き込み予定データ" + str(writeList))
+    print("[log] "+"書き込み予定データ" + str(writeList))
     try:
         with open(CSV_PATH1 + nowDataList[2] + CSV_PATH2, 'a') as f:
             writer = csv.writer(f, lineterminator='\n')  # 改行コード（\n）を指定しておく
-            writer.writerow(writeList)  # list（1次元配列）の場合
-            print("成功")
+            writer.writerow(writeList)  # list（1次元配列)
+            print("[log] "+"成功")
         PlayVoice.playPathById(nowDataList[2])
         PlayVoice.playMorning()
     except:
         PlayVoice.playError_DataError()
-        print("書き込み失敗")
+        print("[error] " + "書き込み失敗")
 
 
 def clockOut(list):
@@ -46,26 +45,26 @@ def clockOut(list):
     before = long(tmp[2])
     tmp[2] = now.strftime("%X")
     tmp.append(str((delta - before) / 60.0))
-    print("書き込み予定データ" + str(tmp))
+    print("[log] "+"書き込み予定データ" + str(tmp))
     try:
         with open(CSV_PATH1 + nowDataList[2] + CSV_PATH2, 'w') as f:
             writer = csv.writer(f, lineterminator='\n')  # 改行コード（\n）を指定しておく
             for i in range(len(list)):
                 writer.writerow(list[i-1])
-        print("書き込み正常終了")
+        print("[log] "+"書き込み正常終了")
         print(nowDataList[2])
         PlayVoice.playPathById(nowDataList[2])
         PlayVoice.playEvening()
     except:
-        print("書き込み失敗")
+        print("[error] " + "書き込み失敗")
         PlayVoice.playError_DataError()
 
 
 def operateCsv():
     print("")
-    print("タイムカードを読み込んでいます。")
+    print("[log] "+"タイムカードを読み込んでいます。")
     path = CSV_PATH1 + nowDataList[2] + CSV_PATH2
-    print("書き込みパス: " + str(path))
+    print("[log] "+"書き込みパス: " + str(path))
     workingList = []
     factor_num = 0
     try:
@@ -77,9 +76,9 @@ def operateCsv():
 
         factor_num = len(workingList[len(workingList) - 1])
     except:
-        print("何もなし")
+        print("[error] " + "何もなし")
         factor_num = 0
-    print("データ数: " + str(factor_num))
+    print("[log] "+"データ数: " + str(factor_num))
     if factor_num == 3:  # 退勤処理
         print("")
         print("**************")
@@ -98,7 +97,7 @@ def operateCsv():
         clockIn()
     else:
         PlayVoice.playError_DataError()
-        print("データがおかしい")
+        print("[error] " + "データがおかしい")
 
 
 def get_keys_from_value(d, val):  # (dictinary, search value)
@@ -116,9 +115,9 @@ def CheckNFC(_idm):
             checkFlg = True
             break
     if checkFlg:
-        print(str(nowDataList[0])+"ありますねえ！")
+        print("[log] "+str(nowDataList[0])+"名前発見")
     else:
-        print("ないです")
+        print("[error] " + "CSV名前なし")
         PlayVoice.playError_NoName()
 
     return checkFlg
@@ -127,7 +126,7 @@ def CheckNFC(_idm):
 def readNameLists():
     print("")
     print("")
-    print("nameListを読み込んでいます。")
+    print("[log] "+"nameListを読み込んでいます。")
     with open(CSV_NAMELIST_PATH, 'r') as f:
         reader = csv.reader(f)
         header = reader.next()
@@ -136,17 +135,17 @@ def readNameLists():
             print(str(line))
             tmpDict[line[1]] = line[2]
             userData[line[0]] = tmpDict
-    print("nameList読み込み終了")
-    print("データ数: "+str(len(userData)))
+    print("[log] "+"nameList読み込み終了")
+    print("[log] "+"データ数: "+str(len(userData)))
     print("")
     print("")
 
 
 def connected(tag):
 
-    print('ICカード検知 Type = {}'.format(tag.type))
+    print("[log] "+'ICカード検知 Type = {}'.format(tag.type))
     idm = binascii.hexlify(tag.identifier).upper()
-    print("idm = %s" % idm)
+    print("[log] "+"idm = %s" % idm)
     if CheckNFC(idm):
         operateCsv()
 
@@ -165,4 +164,4 @@ if __name__ == "__main__":
     clf = nfc.ContactlessFrontend('usb')
     clf.connect(rdwr={'on-connect': connected})
     clf.close()
-    print("待機")
+    print("[log] "+"待機")
---
+++ IVR_timeReport2.py
@@ -0,0 +1,83 @@
+import time
+import pyaudio
+import wave
+import datetime
+import os
+import random
+
+PATH1 = "./voice_time/"
+PATH2 = ".wav"
+
+morningTime = datetime.time(9, 0)
+lunchTime = datetime.time(13, 10)
+breakTime = datetime.time(16, 0)
+eveningTime = datetime.time(19, 0)
+testTime = datetime.time(1, 8)
+
+morningPath = "morning/"
+lunchPath = "lunch/"
+breakPath = "break/"
+eveningPath = "evening/"
+testPath = "test/"
+time_list = {morningTime: morningPath, lunchTime: lunchPath,
+             breakTime: breakPath, eveningTime: eveningPath,
+             testTime: testPath}
+
+
+def playSound(path):
+    CHUNK = 1024
+    filename = path
+    wf = wave.open(filename, 'rb')
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+    data = wf.readframes(CHUNK)
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+
+def selectRandom(name):
+    path = PATH1 + name
+    files = os.listdir(path)
+    files_file = [f for f in files if os.path.isfile(os.path.join(path, f))]
+    return random.choice(files_file)
+
+
+def checkTime(tmp):
+    flg = tmp
+    now = datetime.datetime.now()
+    for i in time_list.keys():
+        if now.hour == i.hour and now.minute == i.minute \
+                and now.second == i.second:
+            if flg:
+                playSound(PATH1 + time_list[i] + selectRandom(time_list[i]))
+                print("[log] " + str(now) + ": " + PATH1 + time_list[i] +
+                      selectRandom(time_list[i]) + "を再生しました。")
+                flg = False
+        else:
+            flg = True
+    return flg
+
+
+if __name__ == "__main__":
+    print("****************************")
+    print("   IVR_TimeReport-System")
+    print("****************************")
+
+    print("")
+    print("時報予定時刻")
+    for k in time_list.keys():
+        print(k)
+    print("")
+    print("時間監視開始")
+    print("")
+    played = True
+    while True:
+        played = checkTime(played)
+        time.sleep(0.5)
